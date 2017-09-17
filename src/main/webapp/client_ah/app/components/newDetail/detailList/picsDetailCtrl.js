5e331a0f0d74bf0f99183531bb726f1e12640147
mengwei2018 <1530426713@qq.com>
2017-09-17 16:44:35 +0800

    细览页修改

--- src/main/webapp/client_ah/app/components/newDetail/detailList/picsDetailCtrl.js
+++ src/main/webapp/client_ah/app/components/newDetail/detailList/picsDetailCtrl.js
@@ -81,6 +81,15 @@ clientModule.controller('picsDetailCtrl', function ($scope, $cookies, req, md5, 
     };
 
 
+    // 细览图片轮播控制
+    $(function () {
+        $timeout(function () {
+            $('.detial_content_pic').slide({
+               mainCell:".pic_show ul",effect:"leftLoop",autoPlay:false
+            });
+        }, 1000);
+    });
+
 
     //判断是否选择了数据
     function judgeIfSelData() {
@@ -106,7 +115,7 @@ clientModule.controller('picsDetailCtrl', function ($scope, $cookies, req, md5, 
     }
 
     //下载图片
-    vm.downPic = function() {
+   /* vm.downPic = function() {
         judgeIfSelData();
         if (vm.selKeyArr.length < 1) {
             layer.alert('请至少选择一个图片进行操作');
@@ -160,12 +169,12 @@ clientModule.controller('picsDetailCtrl', function ($scope, $cookies, req, md5, 
             '?picIds=' + details +
             '&type=' + downWay;
         window.open('/photo/downloadPicture/downByPicId.do' + params, '_blank');
-    }
+    } */
 
     /**
      * 获取网站公告
      */
-    function getWebPublishData() {
+    /*function getWebPublishData() {
         req.post('notice/showToHomePage.do', {}).success(function (resp) {
             if (resp.code == '211') {
                 vm.webPublishArray = resp.data;console.log(resp.data);
@@ -213,10 +222,170 @@ clientModule.controller('picsDetailCtrl', function ($scope, $cookies, req, md5, 
                 layer.alert(resp.msg);
             }
         });
+    }; */
+
+     //下载时切换图文
+    vm.toggleImgTxt = function (downType) {
+        vm.downType = downType;
+    };
+
+    //下载图片
+    vm.downPic = function () {
+        req_genereateOrder();
+    };
+    //订户下载生成订单请求
+    function req_genereateOrder(callback) {
+        req.post('downloadPicture/downForOrder.do', {
+            picIds: vm.pictureId
+        }).success(function (resp) {
+            if (resp.code == '211') {
+                vm.orderList = resp.other;
+                vm.orderId = resp.other.id;
+                vm.pics= resp.data;
+                vm.orderDetailId='';
+                angular.forEach(vm.pics,function (iterator) {
+                    vm.orderDetailId += iterator.id+",";
+                });
+                // alert(vm.orderDetailId);
+                $('#down-pic-order-modal').modal('show');
+            }else if(resp.msg != '未登录'){
+               layer.alert(resp.msg);
+            }
+        });
+    }
+
+    //确定下载图片
+    vm.sureDownPic = function (orderId, downWay) {
+        req_sureDownPic(orderId, downWay);
+    };
+    //确定下载图片请求
+    function req_sureDownPic(orderId, downWay) {
+        var params =
+            '?orderId=' + orderId +
+            '&type=' + downWay;
+        window.open('/photo/downloadPicture/downByOrder.do' + params, '_blank');
+    }
+
+    //根据订单详情id下载图片
+    vm.sureDownPicByOrderDetail = function (details, downWay) {
+        req_sureDownPicByOrderDetailId(details, downWay);
+    };
+    //根据订单详情id下载图片
+    function req_sureDownPicByOrderDetailId(details, downWay) {
+        var params =
+            '?picIds=' + details +
+            '&type=' + downWay;
+        window.open('/photo/downloadPicture/downByPicId.do' + params, '_blank');
+    }
+
+    /**
+     * 获取网站公告
+     */
+    function getWebPublishData() {
+        req.post('notice/showToHomePage.do', {}).success(function (resp) {
+            if (resp.code == '211') {
+                vm.webPublishArray = resp.data;
+                if (vm.webPublishArray.length > 2) {
+                    vm.webPublishArray = vm.webPublishArray.slice(0, 2);
+                }
+            }else if(resp.msg != '未登录'){
+                layer.alert(resp.msg);
+            }
+        });
+    }
+
+    /**
+     * 获取收藏夹
+     */
+    function getCollectBookData() {
+        req.post('favorite/showAllFavoriteFolderName.do', {}).success(function (resp) {
+            if (resp.code == '211') {
+                vm.collectBookArray = resp.data;
+            }else if(resp.msg != '未登录'){
+                layer.alert(resp.msg);
+            }
+        });
+    }
+
+    /**
+     * 点击是否显示收藏夹
+     */
+    vm.IsShowMineCollectFlag = false;
+    vm.onIsShowMineCollectClick = function () {
+        vm.IsShowMineCollectFlag = !vm.IsShowMineCollectFlag;
+        if(vm.IsShowMineCollectFlag){
+            // 获取收藏夹名
+            getCollectBookData();
+        }
+    };
+
+    /**
+     * 添加收藏夹
+     */
+    vm.onAddCollectConClick = function () {
+        if (!vm.addCollectNameModel) {
+            layer.alert('请输入收藏夹名');
+            return;
+        }
+        req.post("favorite/addFavoriteFolder.do", {
+            folderName: vm.addCollectNameModel
+        }).success(function (resp) {
+            if (resp.code == '211') {
+                vm.addCollectNameModel = "";
+                vm.onModalHide('favoriteAddModal');
+                getCollectBookData();
+                layer.msg("添加成功");
+            }else if(resp.msg != '未登录'){
+                layer.alert(resp.msg);
+            }
+        });
+    };
+
+    /**
+     * 将图片加入收藏夹
+     */
+    vm.onCollectPicClick = function (fileId) {
+        req.post("favoriteFolderPic/addFavoriteFolderPic.do", {
+            picid: vm.pictureId,
+            folderId: fileId
+        }).success(function (resp) {
+            if (resp.code == '211') {
+                layer.msg("收藏成功");
+            }else if(resp.msg != '未登录'){
+                layer.alert(resp.msg);
+            }
+        });
+    };
+
+    /**
+     * 将图片加入购物车
+     */
+    vm.onCartPicClick = function () {
+        req.post("car/add.do", {
+            pictureId: vm.pictureId,
+        }).success(function (resp) {
+            if (resp.code == '211') {
+                contacts=resp.data.contacts
+                notSales=resp.data.notSales
+                if(contacts.length!=0 && notSales.length!=0 ){
+                    layer.msg("包含不可出售图，张数用户不能添加定价图片")
+                }else if(contacts.length!=0  ){
+                    layer.msg("张数用户不能添加定价图片")
+                }else if( notSales.length!=0 ){
+                    layer.msg("包含不可出售图")
+                }else{
+                    layer.msg("加入购物车成功");
+                }
+                // layer.msg("加入购物车成功");
+            }else if(resp.msg != '未登录'){
+                layer.alert(resp.msg);
+            }
+        });
     };
 
     // 请求
     getWebPublishData();
+
     //获取首页签发的稿件请求
     req_getClientGroups(25);
     function req_getClientGroups(signId) {
